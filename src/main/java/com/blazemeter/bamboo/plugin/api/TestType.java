a98c7e6090fab29bf73f4d55ea1cbf13c09e3b32
zmicer <dzmitrykashlach@gmail.com>
2016-03-10 14:43:19 +0300

    JEN-134

--- src/main/java/com/blazemeter/bamboo/plugin/api/TestType.java
+++ src/main/java/com/blazemeter/bamboo/plugin/api/TestType.java
@@ -4,5 +4,5 @@ package com.blazemeter.bamboo.plugin.api;
  * Created by zmicer on 6.5.15.
  */
 public enum TestType {
-    http, followme, multi, jmeter,webdriver
+    http, followme, multi, jmeter,webdriver,taurus
 }
