bf0faf1283837bbe802d086440e3267908129692
zmicier <dzmitrykashlach@gmail.com>
2017-12-08 15:59:33 +0300

    clean-up utils

--- src/main/java/com/blazemeter/bamboo/plugin/Utils.java
+++ src/main/java/com/blazemeter/bamboo/plugin/Utils.java
@@ -17,17 +17,10 @@ package com.blazemeter.bamboo.plugin;
 import java.io.*;
 import java.util.*;
 
-import com.atlassian.bamboo.build.logger.BuildLogger;
-import com.atlassian.bamboo.task.TaskContext;
-import net.sf.json.JSONArray;
-import net.sf.json.JSONObject;
-
 public class Utils {
 
-    private Utils(){
-	}
-
-
+    private Utils() {
+    }
     public static String getVersion() {
         Properties props = new Properties();
         try {
@@ -37,40 +30,4 @@ public class Utils {
         }
         return props.getProperty("version");
     }
-
-    public static File resolvePath(TaskContext context, String path, BuildLogger logger) throws Exception {
-        File f = null;
-        File root = new File("/");
-        if (path.equals("/")) {
-            f = root;
-        }
-        if (path.startsWith("/")) {
-            f = new File(root, path);
-        } else {
-            f = new File(context.getWorkingDirectory().getAbsolutePath() + "/build # "
-                + context.getBuildContext().getBuildNumber(), path);
-        }
-        if (!f.exists()) {
-            boolean mkDir = false;
-            try {
-                mkDir = f.mkdirs();
-            } catch (Exception e) {
-                throw new Exception("Failed to find filepath = " + f.getName());
-            } finally {
-                if (!mkDir) {
-                    logger.addBuildLogEntry("Failed to create " + f.getCanonicalPath() + " , workspace will be used.");
-                    f = new File(context.getWorkingDirectory(), path);
-                    f.mkdirs();
-                    logger.addBuildLogEntry("Resolving path into " + f.getCanonicalPath());
-                }
-            }
-        }
-        if (!f.canWrite()) {
-            f = new File(context.getWorkingDirectory(), path);
-            f.mkdirs();
-            logger.addBuildLogEntry("Resolving path into " + f.getCanonicalPath());
-        }
-        return f.getCanonicalFile();
-    }
-
 }
