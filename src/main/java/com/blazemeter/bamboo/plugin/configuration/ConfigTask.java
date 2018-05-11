c602ed8588aa9e57521da1da57c868679db5631c
Artem Fedorov <artem.fedorov@blazemeter.com>
2018-05-11 17:13:49 +0300

    Add workspace selection (#13)
    * add wsp select
    
    * add selection in front-end
    
    * optimize backend
    
    * fixed unselected values
    
    * fixed error messages
    
    * fix NPE in validation
    
    * fixed test selection when it open
    
    * fixed test selection
    
    * changelog

--- src/main/java/com/blazemeter/bamboo/plugin/configuration/ConfigTask.java
+++ src/main/java/com/blazemeter/bamboo/plugin/configuration/ConfigTask.java
@@ -34,13 +34,13 @@ import com.blazemeter.bamboo.plugin.Utils;
 import com.blazemeter.bamboo.plugin.logging.ServerLogger;
 import com.blazemeter.bamboo.plugin.logging.ServerUserNotifier;
 import com.blazemeter.bamboo.plugin.servlet.AdminServlet;
-import com.google.common.collect.LinkedHashMultimap;
 import org.apache.commons.lang.StringUtils;
 
 import java.util.ArrayList;
-import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.TreeMap;
 
 public class ConfigTask extends AbstractTaskConfigurator implements BuildTaskRequirementSupport {
     PluginSettingsFactory pluginSettingsFactory;
@@ -67,7 +67,7 @@ public class ConfigTask extends AbstractTaskConfigurator implements BuildTaskReq
         UserNotifier serverUserNotifier = new ServerUserNotifier();
         Logger logger = new ServerLogger();
         utils = new BambooBzmUtils(apiId, apiSecret, url, url, serverUserNotifier, logger);
-        fillContextWithTests(context);
+        fillContextWithTests(context, null);
         context.put(AdminServlet.URL, url);
         logger.info("New BlazeMeter task is opened for configuration.");
     }
@@ -96,7 +96,7 @@ public class ConfigTask extends AbstractTaskConfigurator implements BuildTaskReq
         Logger logger = new ServerLogger();
         utils = new BambooBzmUtils(psai, psas, pssu, pssu, serverUserNotifier, logger);
         logger.info("BlazeMeter task is opened for configuration.");
-        fillContextWithTests(context);
+        fillContextWithTests(context, config.get("selectedtest"));
     }
 
     /**
@@ -127,29 +127,40 @@ public class ConfigTask extends AbstractTaskConfigurator implements BuildTaskReq
         utils.getNotifier().notifyInfo("BlazeMeter task settings were validated and saved.");
     }
 
-    private void fillContextWithTests(Map<String, Object> context) {
-        LinkedHashMultimap<String, String> testListDropDown = null;
-        User user = null;
+    private void fillContextWithTests(Map<String, Object> context, String selectedTest) {
+        Map<String, String> testListDropDown = new HashMap<>();
+        Map<String, String> workspacesDropDown = new TreeMap<>();
+        Map<String, Map<String, String>> wspMap = new HashMap<>();
         try {
-            user = User.getUser(utils);
+            User user = User.getUser(utils);
             assert user.getId() != null;
-            testListDropDown = testsList();
-            context.put(Constants.TEST_LIST, testListDropDown.asMap());
+            String wspForSelect = fillAccountInfo(user, selectedTest, wspMap, workspacesDropDown, testListDropDown);
+            context.put("savedWsp", wspForSelect);
+            context.put("savedTest", selectedTest);
+
         } catch (Exception e) {
             utils.getLogger().error("Failed to get user: " + e);
-            testListDropDown = LinkedHashMultimap.create(1, 1);
             testListDropDown.put(CHECK_CREDENTIALS, CHECK_CREDENTIALS);
-            context.put(Constants.TEST_LIST, testListDropDown.asMap());
-            return;
+            workspacesDropDown.put(CHECK_CREDENTIALS, CHECK_CREDENTIALS);
+            wspMap.put(CHECK_CREDENTIALS, testListDropDown);
+
+            context.put("savedWsp", CHECK_CREDENTIALS);
+            context.put("savedTest", CHECK_CREDENTIALS);
+        } finally {
+            context.put(Constants.TEST_LIST, testListDropDown);
+            context.put("workspaceList", workspacesDropDown);
+            context.put("wspMap", wspMap);
         }
     }
 
     private void fillErrorCollection(String selectedTest, ErrorCollection errorCollection) {
-        if (StringUtils.isEmpty(selectedTest) | selectedTest.contains(CHECK_CREDENTIALS)) {
+        if (StringUtils.isBlank(selectedTest) || selectedTest.contains(CHECK_CREDENTIALS)) {
             errorCollection.addErrorMessage(CHECK_CREDENTIALS + CHECK_TESTS);
+            return;
         }
         if (selectedTest.contains(WORKSPACE)) {
             errorCollection.addErrorMessage("Cannot save workspace as a test. Please, select correct test.");
+            return;
         }
         if (selectedTest.contains(NO_TESTS)) {
             errorCollection.addErrorMessage("No tests in current workspace. Please, select correct test.");
@@ -201,30 +212,48 @@ public class ConfigTask extends AbstractTaskConfigurator implements BuildTaskReq
         }
     }
 
-    private LinkedHashMultimap<String, String> testsList() throws Exception {
-        LinkedHashMultimap<String, String> testListDropDown = LinkedHashMultimap.create();
-        User user = User.getUser(utils);
+
+    public String fillAccountInfo(User user, String selectedTest,
+                                  Map<String, Map<String, String>> wspMap,
+                                  Map<String, String> workspacesDropDown,
+                                  Map<String, String> testListDropDown) throws Exception {
+
+        String wspForSelect = null;
+
         List<Account> accounts = user.getAccounts();
         for (Account a : accounts) {
+
             List<Workspace> workspaces = a.getWorkspaces();
             List<AbstractTest> tests = new ArrayList<>();
             for (Workspace wsp : workspaces) {
+                workspacesDropDown.put(WORKSPACE + wsp.getId(), wsp.getName() + "(" + wsp.getId() + ")");
                 tests.clear();
                 addMultiTests(wsp, tests);
                 addSingleTests(wsp, tests);
-                Comparator<AbstractTest> c = (AbstractTest t1, AbstractTest t2) -> t1.getName().compareToIgnoreCase(t2.getName());
-                tests.sort(c);
-                testListDropDown.put(WORKSPACE + wsp.getId(), "===" + wsp.getName() + "(" + wsp.getId() + ")===");
+
+                Map<String, String> testsList = new HashMap<>();
                 if (tests.isEmpty()) {
-                    testListDropDown.put("no-tests"+workspaces.indexOf(wsp), "No tests in workspace");
+                    testsList.put("no-tests" + workspaces.indexOf(wsp), "No tests in workspace");
+                    wspMap.put(WORKSPACE + wsp.getId(), testsList);
                     continue;
                 }
+
+                boolean isSelectedWorkspace = false;
                 for (AbstractTest t : tests) {
                     String testIdType = t.getId() + "." + t.getTestType();
-                    testListDropDown.put(testIdType, t.getName() + "(" + testIdType + ")");
+                    testsList.put(testIdType, t.getName() + "(" + testIdType + ")");
+                    if (testIdType.equals(selectedTest)) {
+                        isSelectedWorkspace = true;
+                        wspForSelect = WORKSPACE + wsp.getId();
+                    }
                 }
+                wspMap.put(WORKSPACE + wsp.getId(), testsList);
+                if (isSelectedWorkspace) {
+                    testListDropDown.putAll(testsList);
+                }
+
             }
         }
-        return testListDropDown;
+        return wspForSelect;
     }
 }
--- src/main/resources/templates/editConfigTask.ftl
+++ src/main/resources/templates/editConfigTask.ftl
@@ -1,7 +1,97 @@
-[@ww.select cssClass="builderSelectWidget" labelKey='blazemeter.test' name='selectedtest' list='testlist' required='true' emptyOption='false'/]
+<script>
+    var savedWsp = "${savedWsp}";
+    var savedTest = "${savedTest}";
+
+    var bzmTestMap = [];
+
+    [#foreach key in wspMap.keySet()]
+        var array = [];
+        [#foreach id in wspMap.get(key).keySet()]
+            var obj = {};
+            obj.id = "${id}";
+            obj.value = "${wspMap.get(key).get(id)}";
+            array.push(obj);
+        [/#foreach]
+        array.sort(customComparator);
+        bzmTestMap["${key}"] = array;
+    [/#foreach]
+
+    function customComparator(a, b) {
+        var valueA = a.value.toUpperCase(); // ignore upper and lowercase
+        var valueB = b.value.toUpperCase(); // ignore upper and lowercase
+        if (valueA < valueB) {
+            return -1;
+        }
+        if (valueA > valueB) {
+            return 1;
+        }
+
+        // names must be equal
+        return 0;
+    }
+
+    function onChangedWorkspaceSelect(wsp) {
+        if (wsp.value) {
+            checkForEmptyValue(wsp);
+            var list = bzmTestMap[wsp.value];
+
+            var testsSel = document.getElementById("selectedtest");
+            testsSel.options.length = 0;
+
+            for (var i = 0; i < list.length; i++) {
+                var option = new Option(list[i].value, list[i].id)
+                testsSel.options[i] = option;
+            }
+        } else {
+            wsp.prepend(new Option("Select Workspace", ""));
+            wsp.value = "";
+            var testsSel = document.getElementById("selectedtest");
+            testsSel.prepend(new Option("No Workspace", ""));
+            testsSel.value = "";
+        }
+    }
+
+    function checkForEmptyValue(wsp) {
+        for (var i = 0; i < wsp.options.length; i++) {
+            if (wsp.options[i].value == "") {
+                wsp.options[i] = null;
+            }
+        }
+    }
+</script>
+
+[@ww.select labelKey='blazemeter.workspace'
+            name='selectedWorkspace'
+            list='workspaceList'
+            toggle='true'
+            onchange='onChangedWorkspaceSelect(this);'
+/]
+[@ww.select labelKey='blazemeter.test' name='selectedtest' list='testlist'/]
+
+
+
 [@ww.textfield labelKey="blazemeter.config.jmeterProps" name="jmeter.properties" required='false'/]
 [@ww.textarea labelKey="blazemeter.config.notes" name="notes" required='false'/]
 [@ww.checkbox labelKey="blazemeter.config.jtl" name='jtl.report' toggle='true' description=''/]
 [@ww.textfield labelKey="blazemeter.config.jtl.path" name="jtl.path" required='false'/]
 [@ww.checkbox labelKey="blazemeter.config.junit" name='junit.report' toggle='true' description=''/]
 [@ww.textfield labelKey="blazemeter.config.junit.path" name="junit.path" required='false'/]
+
+<script>
+    var wspSel = document.getElementById("selectedWorkspace");
+    wspSel.value = savedWsp;
+    onChangedWorkspaceSelect(wspSel);
+
+    // select value in tests selection
+    var testsSelect = document.getElementById("selectedtest");
+    var isSelected = false;
+    for (var i = 0; i < testsSelect.options.length; i++) {
+        if (testsSelect.options[i].value == savedTest) {
+            testsSelect.options[i].selected = true;
+            isSelected = true;
+        }
+    }
+    if (!isSelected && testsSelect.options.length > 0) {
+        testsSelect.options[0].selected = true;
+    }
+</script>
